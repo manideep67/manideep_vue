933b79e577cf7439f9072a27db7327b7518c9d7b
manideep67 <94004479+manideep67@users.noreply.github.com>
2022-12-07 08:47:18 +0530

    This my first commit

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="container">
+    <HeaderTop TitleName="TOP POPULATED CITIES IN THE WORLD"  />
+    <div class="About">
+    <h1> <button><a href="https://manideepcharugundla.herokuapp.com/"> About Me </a></button> </h1>
+    </div>
+    <PopulationBox :population="population"  />
+    
+  </div>
+</template>
+<script>
+import HeaderTop from './components/Header.vue'
+import PopulationBox from './components/PopulationBox.vue'
+export default {
+  name: 'App',
+  components: {
+    HeaderTop,
+    PopulationBox
+  },
+  data(){
+    return {
+      population: []
+    }
+  },
+  methods: {
+      async fetchpopulation(){
+        const res= await fetch('https://manideepcharugundla.herokuapp.com/api')
+         const data= await res.json()
+          console.log(data)
+         return data.population
+      }
+
+  },
+async created(){
+    this.population = await this.fetchpopulation()
+  }
+}
+</script>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0px;
+}
+body {
+  font-family: 'Montserrat', sans-serif;
+}
+.container {
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 0.3em solid black;
+  padding: 40px;
+  border-radius: 5px;
+  width:100%;
+  background-color: rgb(11, 11, 11);
+  
+
+}
+div{
+  margin-bottom: 0.5em;
+  padding:10px;
+}
+.About{
+  width:100%;
+}
+a{
+    text-decoration: none;
+    color: rgb(2, 7, 16);
+}
+button{
+    font-size:30px;
+    font-family:'Times New Roman', Times, serif;
+    background-color: rgb(193, 218, 224);
+    width:100%;
+    border:3px solid rgb(46, 99, 173);
+}
+</style>
---
+++ src/components/PopulationBox.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="populationbox">
+       <div v-for="population in population" :key="population.id" >
+               <PopulaTion :population="population"/>
+       </div>
+    </div>
+</template>
+<script>
+import PopulaTion from "./Population.vue"
+export default{
+   name: 'PopulationBox',
+   components: {
+      PopulaTion
+   },
+   props: {
+    population: Array
+   }
+}
+</script>
+<style>
+.populationbox{
+   padding: 40px;
+   display: grid;
+   grid-template-columns: 1fr 1fr 1fr;
+   border:3px solid rgb(11, 12, 97);
+   background-repeat: no-repeat;
+   background-attachment: fixed;  
+   background-size: cover;
+   background-image: url("https://ourworldindata.org/uploads/2019/10/Screen-Shot-2019-10-31-at-12.02.19-768x527.png");
+}
+</style>
